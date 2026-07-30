78f3bcee5e86c6d43e4d2b3964cb53143fe8982c
KeshavSharma-dotcom <keshavsharma787705@gmail.com>
2026-07-30 22:35:11 +0530

    Home Page and Styles

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import "../assets/styles/Home.css"
+import { useNavigate } from "react-router"
+
+export default function Home() {
+    const navigate = useNavigate()
+
+    return (
+        <div className="home">
+            <div className="background-effects">
+                <div className="flames">
+
+                    {[...Array(20)].map((_, i) =>
+
+                        <span
+                            key={i}
+                            style={{
+                                left: `${i * 5}%`,
+                                animationDelay: `-${i * .35}s`
+                            }}
+                        />
+
+                    )}
+
+                </div>
+                <div className="embers"></div>
+                <div className="fog"></div>
+            </div>
+
+            <div className="home-overlay">
+                <h1 className="home-title">Vinshades</h1>
+                <p className="home-subtitle">Explore webgames</p>
+                <div className="games-section">
+                    <button className="game-card" onClick={() => navigate("/mystic-kitchen")}>
+                        <div className="game-glow"></div>
+                        <h2 className="game-title">
+                            Mystic Kitchen
+                        </h2>
+                        <p className="game-description">
+                            Master enchanted recipes, serve mystical creatures,
+                            and build the greatest magical kitchen.
+                        </p>
+                        <span className="game-button">
+                            Play →
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
